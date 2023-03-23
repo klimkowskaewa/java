8d438a823b3e666959987d6a67d612226556734a
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-03-23 21:16:13 +0100

    comment

---
+++ src/MMusicPlayer.java
@@ -0,0 +1,30 @@
+public abstract class MMusicPlayer implements MPlayer{
+    private MSong[] library;
+    private int current;
+
+    public MMusicPlayer(MSong[] library) {
+        this.library = library;
+        this.current = 0;
+    }
+
+    public MSong getCurrentSong() {
+        return library[current];
+    };
+
+    @Override
+    public void next() {
+        if(++current >= library.length) {
+            current = 0;
+        }
+        play();
+    }
+
+    @Override
+    public void previous() {
+        if(--current < 0 ) {
+            current = library.length - 1;
+        }
+        play();
+    }
+
+}
