26b6d2736203b579ca301b916affcec6fded1c48
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-03-24 20:15:54 +0100

    farm

---
+++ src/FarmDog.java
@@ -0,0 +1,15 @@
+public class FarmDog extends FarmAnimal{
+    public FarmDog(String name) {
+        this.name = name;
+    }
+
+    @Override
+    String showName() {
+        return "dog " + name;
+    }
+
+    @Override
+    String makeSound() {
+        return "woof woof";
+    }
+}
