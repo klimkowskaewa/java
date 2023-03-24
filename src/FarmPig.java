75815ece5dcc88e84080d2c6d073b3a40336952c
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-03-24 20:16:04 +0100

    farm

---
+++ src/FarmPig.java
@@ -0,0 +1,15 @@
+public class FarmPig extends FarmAnimal{
+    public FarmPig(String name) {
+        this.name = name;
+    }
+
+    @Override
+    String showName() {
+        return "pig " + name;
+    }
+
+    @Override
+    String makeSound() {
+        return "oink oink";
+    }
+}
