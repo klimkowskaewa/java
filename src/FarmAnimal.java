f165a18962120ff168470d4d626eea4121d9bbf0
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-03-24 20:15:24 +0100

    farm

---
+++ src/FarmAnimal.java
@@ -0,0 +1,19 @@
+public class FarmAnimal {
+    String name;
+
+    public FarmAnimal() {
+        this.name = "no name";
+    }
+
+    String showName() {
+        return "animal " + name;
+    }
+
+    String makeSound() {
+        return "???";
+    }
+
+    void introduce() {
+        System.out.println("I am " + showName() + ": " + makeSound());
+    }
+}
