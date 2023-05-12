2bb3142947d8e2ed1b9f894d9b8fc5a82c320f6a
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-05-12 21:49:50 +0200

    math update

---
+++ src/math/ReverseIntegerNumber.java
@@ -0,0 +1,27 @@
+package math;
+
+public class ReverseIntegerNumber {
+    public static void main(String[] args) {
+        int number = 1234;
+        int positive = 1;
+        if (number < 0 ) {
+            positive = -1;
+            number = -1 * number;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            int x = number % 10;
+
+            int maxDiff = Integer.MAX_VALUE - sum * 10;
+            if (sum > Integer.MAX_VALUE / 10 || x > maxDiff)
+                System.out.println("Wrong number!");
+
+            sum = sum * 10 + x;
+            number /= 10;
+        }
+
+        System.out.println(positive * sum);
+
+    }
+}
