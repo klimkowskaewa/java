13bc10e21aa54565cab6f48130451f2af37e338c
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-06-08 23:22:14 +0200

    collection update

---
+++ src/collections/Furniture.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.Objects;
+
+public class Furniture {
+    public static void main(String[] args) {
+        Chair chair1 = new Chair("Adde", "IKEA", 2016);
+        Chair chair2 = new Chair("Adde", "IKEA", 2016);
+
+        System.out.println(chair1.hashCode());
+        System.out.println(chair2.hashCode());
+
+        System.out.println(chair1.equals(chair2));
+
+    }
+}
+
