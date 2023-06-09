11a117bdbb3bd51f34fbb5952b64a46f81166848
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-06-09 23:51:43 +0200

    collection update

---
+++ src/collections/Demo.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("bbb");
+        list.add("ccc");
+
+        System.out.println(Collections.frequency(list, "bbb"));
+        System.out.println(list);
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
+}
