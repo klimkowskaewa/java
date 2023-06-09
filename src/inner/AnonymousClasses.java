528e9f4820ec7ccc2abf48c3e14aff333163a73b
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-06-10 00:03:16 +0200

    inner update

---
+++ src/inner/AnonymousClasses.java
@@ -0,0 +1,39 @@
+package inner;
+
+public class AnonymousClasses {
+    public interface GreetingModule {
+        void sayHello();
+    }
+    public static class Robot{
+        private final GreetingModule greetingModule;
+        public Robot(GreetingModule greetingModule) {
+            this.greetingModule = greetingModule;
+        }
+        public void saySomething() {
+            greetingModule.sayHello();
+        }
+    }
+    public static void main(String[] args) {
+        Robot jan = new Robot(new GreetingModule() {
+            @Override
+            public void sayHello() {
+                System.out.println("dzień dobry");
+            }
+        });
+        Robot john = new Robot(new GreetingModule() {
+            @Override
+            public void sayHello() {
+                System.out.println("good morning");
+            }
+        });
+        Robot johan = new Robot(new GreetingModule() {
+            @Override
+            public void sayHello() {
+                System.out.println("guten tag");
+            }
+        });
+        jan.saySomething();
+        john.saySomething();
+        johan.saySomething();
+    }
+}
